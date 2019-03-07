536d7dce37ba3003c3c7b6948ddebc2dcb536ee5
Afonso <endmy@life.pls>
2019-03-07 22:20:02 +0000

    ex 3

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,6 +6,10 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            //formatação de strings (ex 3 - variáveis)
+            double xx = 0.12345;
+            int ii = 18;
+
             //string 1 normal (ex 1)
             string message1 = "Ola mundo";
             //string 2 normal (ex 1)
@@ -14,12 +18,22 @@ namespace VariasStrings
             string message3 = @"\n verbatim";
             //string 4 string format (ex 2 [incomplete])
             string message4 = String.Format("Ola {0} bem vindo ao programa número {1}", 8, 6);
+            //string 5 e 6 formatação de strings (ex 3)
+            string message5 = $"xx = {xx:f2}";
+            string message6 = String.Format("xx em percentagem = {0:p1}",xx);
+            //string 7 e 8 formataçãod e strings (ex 3)
+            string message7 = $"ii hexadecimal = {ii:x}";
+            string message8 = $"ii em moeda = {ii:c}";
 
             //imprime as strings
             Console.WriteLine(message1);
             Console.WriteLine(message2);
             Console.WriteLine(message3);
             Console.WriteLine(message4);
+            Console.WriteLine(message5);
+            Console.WriteLine(message6);
+            Console.WriteLine(message7);
+            Console.WriteLine(message8);
             Console.ReadKey();
         }
     }

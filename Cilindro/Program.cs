74e154382341d732f36bb0de45f218bc63f0ac74
Afonso <endmy@life.pls>
2019-03-07 23:28:33 +0000

    cilindro pede variaveis

---
+++ Cilindro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declarar variáveis
+            string alturaS;
+            string raioS;
+            //pedir altura
+            Console.WriteLine("Introduza a altura:\n");
+            alturaS = Console.ReadLine();
+            //converter altura em float
+            //pedir raio
+            Console.WriteLine("Introduza o raio:\n");
+            raioS = Console.ReadLine();
+            //converter raio em float
+            //calcular volume
+            //calcular area superficial
+            //apresnetar os resultados
+            //debug neste pc de merda
+            Console.ReadKey();
+        }
+    }
+}

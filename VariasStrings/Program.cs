bc91f145780052defa40ed41428deef40a976dd4
Afonso <endmy@life.pls>
2019-03-07 21:08:45 +0000

    varias strings

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,7 +6,13 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string message1 = "Ola mundo";
+            string message2 = "Ade\nus";
+            string message3 = @"\n verbatim";
+
+            Console.WriteLine(message1);
+            Console.WriteLine(message2);
+            Console.WriteLine(message3);
         }
     }
 }

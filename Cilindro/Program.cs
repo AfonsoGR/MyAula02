07087588ca6c5efbdc1661266a38a7e11822fd4e
Afonso <endmy@life.pls>
2019-03-07 23:46:57 +0000

    cilindro completo

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -7,19 +7,26 @@ namespace Cilindro
         static void Main(string[] args)
         {
             //declarar variáveis
-            string alturaS;
-            string raioS;
+            string alturaS, raioS;
+            float alturaF, raioF, volume, areasup;
+            float pi = 3.1415926f;
             //pedir altura
             Console.WriteLine("Introduza a altura:\n");
             alturaS = Console.ReadLine();
             //converter altura em float
+            alturaF = Convert.ToSingle(alturaS);
             //pedir raio
             Console.WriteLine("Introduza o raio:\n");
             raioS = Console.ReadLine();
             //converter raio em float
+            raioF = Convert.ToSingle(raioS);
             //calcular volume
+            volume = pi * raioF * raioF * alturaF;
             //calcular area superficial
-            //apresnetar os resultados
+            areasup = 2 * pi * raioF * (raioF * alturaF);
+            //apresentar os resultados
+            Console.WriteLine($"O volume é {volume:f3}");
+            Console.WriteLine("A área de superfície é {0:f3}",areasup);
             //debug neste pc de merda
             Console.ReadKey();
         }

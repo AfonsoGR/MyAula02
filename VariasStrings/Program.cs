a5a8445620f344d45602f471af9942f19c8d064a
Afonso <endmy@life.pls>
2019-03-07 21:46:10 +0000

    added strings incomplete

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -6,13 +6,21 @@ namespace VariasStrings
     {
         static void Main(string[] args)
         {
+            //string 1 normal
             string message1 = "Ola mundo";
+            //string 2 normal
             string message2 = "Ade\nus";
+            //string 3 verbatim
             string message3 = @"\n verbatim";
+            //string 4 string format
+            string message4 = String.Format("Ola {0} bem vindo ao programa número {1}", 8, 6);
 
+            //imprime as strings
             Console.WriteLine(message1);
             Console.WriteLine(message2);
             Console.WriteLine(message3);
+            Console.WriteLine(message4);
+            Console.ReadKey();
         }
     }
 }

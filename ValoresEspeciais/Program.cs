53d4fbb1a6734a861e2d37ea81da3f52ab7bac1a
Afonso <endmy@life.pls>
2019-03-08 00:22:29 +0000

    valores especiais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //valores maximos e minimos
+            sbyte sbmax = sbyte.MaxValue;
+            double dbmax = double.MaxValue;
+            ulong ulmax = ulong.MaxValue;
+
+            Console.WriteLine(sbmax);
+            Console.WriteLine(dbmax);
+            Console.WriteLine(ulmax);
+
+            //f5 up in da house
+            Console.ReadKey();
+        }
+    }
+}
